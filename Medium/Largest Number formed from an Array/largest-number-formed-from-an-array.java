b981146a9ab9f862164dd1eb6fcd992af310fbb3
dilkhush2001 <140965939+dilkhush2001@users.noreply.github.com>
2024-03-03 17:18:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest Number formed from an Array/largest-number-formed-from-an-array.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] arr = br.readLine().trim().split(" ");
+
+            String ans = new Solution().printLargest(n, arr);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String printLargest(int n, String[] arr) {
+        Arrays.sort(arr, (a, b) -> {
+            String s1 = a+b;
+            String s2 = b+a;
+            return s2.compareTo(s1);
+        });
+        StringBuilder sb=new StringBuilder();
+        for(String ele: arr)sb.append(ele);
+        return sb.toString();
+    }
+}
