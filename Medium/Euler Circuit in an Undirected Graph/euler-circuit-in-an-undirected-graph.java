0ce799118d52edd07c334a8589722ff5d1e53688
dilkhush2001 <140965939+dilkhush2001@users.noreply.github.com>
2024-03-29 23:18:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Euler Circuit in an Undirected Graph/euler-circuit-in-an-undirected-graph.java
+++ Medium/Euler Circuit in an Undirected Graph/euler-circuit-in-an-undirected-graph.java
@@ -36,8 +36,6 @@ class GFG {
 
 // User function Template for Java
 
-
-
 class Solution {
     public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
         for(int i=0;i<v;i++)if(adj.get(i).size()%2!=0)return false;
