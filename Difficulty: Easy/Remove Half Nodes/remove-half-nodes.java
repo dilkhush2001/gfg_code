bfcba7af9f98bf04caa351193eaee90044ef7859
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-07-20 22:53:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Half Nodes/remove-half-nodes.java
+++ Difficulty: Easy/Remove Half Nodes/remove-half-nodes.java
@@ -123,13 +123,14 @@ class Node{
 class Solution {
     // Return the root of the modified tree after removing all the half nodes.
     public Node RemoveHalfNodes(Node root) {
-        if (root == null) return null;
-        if (root.left == null && root.right == null) return root;
-        if (root.left == null) return RemoveHalfNodes(root.right);
-        if (root.right == null) return RemoveHalfNodes(root.left);
-
-        root.left = RemoveHalfNodes(root.left);
-        root.right = RemoveHalfNodes(root.right);
+        if(root==null)return null;
+        if(root.left==null && root.right==null)return root;
+        if(root.left==null)return RemoveHalfNodes(root.right);
+        if(root.right==null)return RemoveHalfNodes(root.left);
+        
+        root.left=RemoveHalfNodes(root.left);
+        root.right=RemoveHalfNodes(root.right);
         return root;
+        
     }
 }
