e78bdc4da4bce87202f4c7899cab46992ac483ae
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-09-28 18:56:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    int[] dp;
+    public int minimizeCost(int k, int arr[]) {
+        dp=new int[arr.length];
+        Arrays.fill(dp, -1);
+        return find(k, arr, 0);
+    }
+    private int find(int k, int[] arr, int idx){
+        if(idx==arr.length-1){
+            return 0;
+        }
+        if(dp[idx]!=-1)return dp[idx];
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<=k && i+idx<arr.length;i++){
+            int take=Math.abs(arr[idx]-arr[i+idx])+find(k, arr, i+idx);
+            min=Math.min(min, take);
+        }
+        return dp[idx]=min;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.minimizeCost(k, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
