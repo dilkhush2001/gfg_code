53fc2f5311cf40e71d15e8244a31a7e0061afd46
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-10-03 17:25:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Vote/majority-vote.java
+++ Difficulty: Medium/Majority Vote/majority-vote.java
@@ -32,15 +32,37 @@ public class Main {
 class Solution {
     // Function to find the majority elements in the array
     public List<Integer> findMajority(List<Integer> arr) {
-        Set<Integer> ans=new HashSet<Integer>();
-        HashMap<Integer, Integer> hm=new HashMap<>();
-       int n=arr.size()/3, c=1;
-       
-        for(int w : arr)hm.put(w, hm.getOrDefault(w, 0)+1);
-        for(Map.Entry<Integer, Integer> entry : hm.entrySet()){
-            if(entry.getValue()>n)ans.add(entry.getKey());
+        int n1=0, n2=0, c1=0, c2=0;
+        for(int ele : arr){
+            if(ele==n1)c1++;
+            else if(ele==n2)c2++;
+            else if(c1==0){
+                n1=ele;
+                c1=1;
+            }
+            else if(c2==0){
+                n2=ele;
+                c2=1;
+            }
+            else{
+                c1--;
+                c2--;
+            }
         }
+        
+        c1=0; c2=0;
+        for(int ele : arr){
+            if(ele==n1)c1++;
+            else if(ele==n2)c2++;
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(c1>arr.size()/3)ans.add(n1);
+        if(c2>arr.size()/3)ans.add(n2);
+        
         if(ans.size()==0)ans.add(-1);
-        return new ArrayList<>(ans);
+        Collections.sort(ans);
+        return ans;
+           
     }
 }
+
