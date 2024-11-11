7a3e737aaa35791b3316548cd3973a728a9ab061
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-11-11 22:43:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make array elements unique/make-array-elements-unique.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            int A = obj.minIncrements(a);
+            System.out.println(A);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int minIncrements(int[] arr) {
+       Arrays.sort(arr);
+       int min=0;
+       int next=arr[0];
+       for(int ele : arr){
+           if(ele<next){
+               min+=next-ele;
+           }
+           next=Math.max(ele+1, next+1);
+       }
+       return min;
+    }
+}
