c2587dbd91ff3ea67e81f7ba487da02f9ccbaa07
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-11-30 18:06:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+        int[] arr=new int[26];
+        for(char ch : s1.toCharArray()){
+            arr[ch-'a']++;
+        }
+        for(char ch : s2.toCharArray()){
+            arr[ch-'a']--;
+        }
+        for(int ele : arr)if(ele!=0)return false;
+        return true;
+    }
+}
