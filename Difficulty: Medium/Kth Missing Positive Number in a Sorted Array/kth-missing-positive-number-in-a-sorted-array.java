0e570f96fbc87391c98db80343731f2065e1c3af
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-12-19 22:56:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.kthMissing(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int max=arr[0];
+        for(int ele : arr)max=Math.max(max, ele);
+        
+        for(int i=1;i<=max;i++){
+            if(!binarySerch(i, arr)){
+                k--;
+            }
+            if(k==0)return i;
+        }
+        return max+k;
+    }
+    boolean binarySerch(int tar, int[] arr){
+        int left=0;
+        int right=arr.length-1;
+        
+        while(left<=right){
+            int mid=(left+right)/2;
+            
+            if(arr[mid]==tar)return true;
+            if(arr[mid]<tar)left=mid+1;
+            else right=mid-1;
+        }
+        return false;
+    }
+}
