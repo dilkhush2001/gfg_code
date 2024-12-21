17816e34a7e01d159f2d0833092e82d79bce04cd
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-12-21 21:05:01 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int matrix[][] = new int[n][n];
+            // String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) System.out.print(matrix[i][j] + " ");
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][]) {
+        int n=matrix.length;
+         for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n;j++){
+                int temp = matrix[n-1-i][j];
+                matrix[n-1-i][j]=matrix[i][j];
+                matrix[i][j]= temp;
+            }
+        }
+    }
+}
