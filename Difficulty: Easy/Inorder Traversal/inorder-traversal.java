4735ceff0014a034190a5c553468d5e7ec6d22d7
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2025-02-07 19:18:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Inorder Traversal/inorder-traversal.java
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.java
@@ -99,19 +99,6 @@ class GfG {
 
 
 
-
-// User function Template for Java
-
-/* A Binary Tree node
-
-class Node {
-    int data;
-    Node left, right;
-   Node(int item)    {
-        data = item;
-        left = right = null;
-    }
-} */
 class Solution {
     public static void method(Node root,ArrayList<Integer> ar){
         if(root==null)
