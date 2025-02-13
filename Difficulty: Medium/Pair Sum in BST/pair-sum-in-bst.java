7a6f22c2e082f72185d1993511d4b656c45f97c3
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2025-02-13 08:49:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.java
+++ Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.java
@@ -92,18 +92,6 @@ class GFG {
 
 // } Driver Code Ends
 
-
-/*
-class Node {
-    int data;
-    Node left, right;
-
-    public Node(int d) {
-        data = d;
-        left = right = null;
-    }
-}
-*/
 class Solution {
     Set<Integer> set=new HashSet<>();
     boolean findTarget(Node root, int target) {
