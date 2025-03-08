94d99f9331fd3653c6c09c90b172edbe8bce19e1
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2025-03-08 23:45:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestPalindrome(S));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution{
+    static String longestPalindrome(String S){
+        String ans="";
+        for(int i=0;i<S.length();i++){
+            String oddP=find(S, i, i);
+            String evenP=find(S, i, i+1);
+            
+            oddP=oddP.length()>evenP.length()?oddP: evenP;
+            
+            if(oddP.length()>ans.length()){
+                ans=oddP;
+            }
+        }
+        return ans;
+        
+    }
+    private static String find(String s, int i, int j){
+        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
+        }
+        return s.substring(i+1, j);
+    } 
+}
